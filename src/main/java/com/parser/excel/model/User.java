5372dc0d9b78c699c4d4dee8f031bb4fe0d781bb
Nitin Baghel <bhnit625@gmail.com>
2024-07-31 20:12:22 +0530

    updated user getter setter

--- src/main/java/com/parser/excel/model/User.java
+++ src/main/java/com/parser/excel/model/User.java
@@ -1,20 +1,44 @@
 package com.parser.excel.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 public class User {
     
-    private String name;
+    public User(String name, String mobileNumber, String requirement, String email) {
+		super();
+		this.name = name;
+		this.mobileNumber = mobileNumber;
+		this.requirement = requirement;
+		this.email = email;
+	}
+	private String name;
     private String mobileNumber;
     private String requirement;
     private String email;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public User() {
+		super();
+	}
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	public String getRequirement() {
+		return requirement;
+	}
+	public void setRequirement(String requirement) {
+		this.requirement = requirement;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
